62c8292529fc321928ba75bc79de7da9ac3dbe46
Shuhao <shuhao@ctssea.com>
2018-03-12 08:37:13 +0000

    Updated ReqT.py Add Server

--- ReqT.py
+++ ReqT.py
@@ -2,16 +2,16 @@ import requests
 import execjs
 os.environ["NODE_PATH"] = os.get
 
-temperature = requests.get('https://us.wio.seeed.io/v1/node/GroveTempHumD0/temperature?access_token=760369546a5f1ff89282dfed8eac9d7d')
+temperature = requests.get('https://52.221.189.109/v1/node/GroveTempHumD0/temperature?access_token=760369546a5f1ff89282dfed8eac9d7d')
 print(temperature.content)
-humidty = requests.get('https://us.wio.seeed.io/v1/node/GroveTempHumD0/humidity?access_token=760369546a5f1ff89282dfed8eac9d7d')
+humidty = requests.get('https://52.221.189.109/v1/node/GroveTempHumD0/humidity?access_token=760369546a5f1ff89282dfed8eac9d7d')
 print(humidty.content)
-dust = requests.get('https://us.wio.seeed.io/v1/node/GroveLuminanceA0/luminance?access_token=760369546a5f1ff89282dfed8eac9d7d')
+dust = requests.get('https://52.221.189.109/v1/node/GroveLuminanceA0/luminance?access_token=760369546a5f1ff89282dfed8eac9d7d')
 print(dust.content)
-temperature = requests.get('https://us.wio.seeed.io/v1/node/GroveTempHumD0/temperature?access_token=760369546a5f1ff89282dfed8eac9d7d')
+temperature = requests.get('https://52.221.189.109/v1/node/GroveTempHumD0/temperature?access_token=760369546a5f1ff89282dfed8eac9d7d')
 print('temperature.content')
-humidty = requests.get('https://us.wio.seeed.io/v1/node/GroveTempHumD0/humidity?access_token=760369546a5f1ff89282dfed8eac9d7d')
+humidty = requests.get('https://52.221.189.109/v1/node/GroveTempHumD0/humidity?access_token=760369546a5f1ff89282dfed8eac9d7d')
 print('humidty.content')
-dust = requests.get('https://us.wio.seeed.io/v1/node/GroveLuminanceA0/luminance?access_token=760369546a5f1ff89282dfed8eac9d7d')
+dust = requests.get('https://52.221.189.109/v1/node/GroveLuminanceA0/luminance?access_token=760369546a5f1ff89282dfed8eac9d7d')
 print('dust.content')
 
